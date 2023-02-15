7f163b7bf968b5e98e7be6fc17438bf930547fdf
Verulyrus <merwou@hotmail.com>
2023-02-15 16:02:56 +0100

    Added block registry

--- src/main/java/net/murren/technicalfarms/TechnicalFarmsMod.java
+++ src/main/java/net/murren/technicalfarms/TechnicalFarmsMod.java
@@ -5,10 +5,10 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static net.murren.technicalfarms.registers.BlockRegister.registerBlocks;
+
 public class TechnicalFarmsMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "technicalfarms";
 	public static final Logger LOGGER = LoggerFactory.getLogger("technicalfarms");
 
 	@Override
@@ -16,7 +16,7 @@ public class TechnicalFarmsMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		registerBlocks();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/net/murren/technicalfarms/registers/BlockRegister.java
+++ src/main/java/net/murren/technicalfarms/registers/BlockRegister.java
@@ -1,4 +1,22 @@
 package net.murren.technicalfarms.registers;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.Material;
+import net.murren.technicalfarms.blocks.ExtractorBlock;
+
+import static net.murren.technicalfarms.TechnicalFarmsMod.MOD_ID;
+
 public class BlockRegister {
+    public static final Block EXTRACTOR_BLOCK = registerBlock("extractor_block",
+            new ExtractorBlock(FabricBlockSettings.of(Material.WOOD)));
+
+    public static Block registerBlock(String name, Block block)
+    {
+        return Registry.register(Registry.BLOCK, new ResourceLocation(MOD_ID, name), block);
+    }
+
+    public static void registerBlocks() {}
 }
