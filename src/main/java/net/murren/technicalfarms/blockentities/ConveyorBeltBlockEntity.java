9475c85197735e425274ab2305ed0d17fb18edee
Verulyrus <merwou@hotmail.com>
2023-03-08 16:01:44 +0100

    added conveyorblock models and textures

--- src/main/java/net/murren/technicalfarms/blockentities/ConveyorBeltBlockEntity.java
+++ src/main/java/net/murren/technicalfarms/blockentities/ConveyorBeltBlockEntity.java
@@ -9,6 +9,7 @@ import net.minecraft.world.phys.AABB;
 
 import java.util.List;
 
+import static net.minecraft.world.level.block.state.properties.BlockStateProperties.*;
 import static net.murren.technicalfarms.registers.BlockEntityTypes.CONVEYOR;
 
 public class ConveyorBeltBlockEntity extends BlockEntity {
@@ -18,7 +19,12 @@ public class ConveyorBeltBlockEntity extends BlockEntity {
     }
 
     public static void tick(Level level, BlockPos pos, BlockState state1, ConveyorBeltBlockEntity be) {
-        List<ItemEntity> items = level.getEntitiesOfClass(ItemEntity.class, new AABB(pos.above()));
-
+        List<ItemEntity> items = level.getEntitiesOfClass(ItemEntity.class, new AABB(pos));
+        switch (state1.getValue(FACING)) {
+            case NORTH -> items.forEach(item -> item.lerpMotion(0, 0, -1));
+            case EAST -> items.forEach(item -> item.lerpMotion(1, 0, 0));
+            case SOUTH -> items.forEach(item -> item.lerpMotion(0, 0, 1));
+            case WEST -> items.forEach(item -> item.lerpMotion(-1, 0, 0));
+        }
     }
 }
